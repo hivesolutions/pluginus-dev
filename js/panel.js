39ee8933b78455520dba6319098a6ed126b9a80e
joamag <joamag@hive.pt>
2019-06-21 20:43:24 +0100

    Better code format

--- js/panel.js
+++ js/panel.js
@@ -1,16 +1,19 @@
-window.onload = function () {
+window.onload = function() {
     chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
         if (changeInfo.status !== "complete") {
             return;
         }
         chrome.devtools.inspectedWindow.eval(
             "[manager.version, manager.plugins.map(v => v.getName())]",
-            function (result, err) {
-                if(err && err.isException) {
+            function(result, err) {
+                if (err && err.isException) {
                     document.getElementById("plugins").innerHTML = "No Pluginus available";
                 } else {
                     [version, plugins] = result;
-                    const info = {version, plugins}
+                    const info = {
+                        version,
+                        plugins
+                    }
                     showInfo(info);
                 }
             }
@@ -18,7 +21,7 @@ window.onload = function () {
     });
 };
 
-const showInfo = function (info) {
+const showInfo = function(info) {
     const pluginsElement = document.getElementById("plugins");
     pluginsElement.innerHTML = "";
 
