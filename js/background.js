db79a6cc1604a0028865b81ed494755072d51a85
joamag <joamag@gmail.com>
2019-06-28 13:22:13 +0100

    New docs added

--- js/background.js
+++ js/background.js
@@ -1,5 +1,15 @@
+
+/**
+ * The main message handler method that will receive all of the
+ * messages sent to this background script.
+ * 
+ * @param {Request} request The request that is being sent.
+ * @param {Element} sender The sender entity reponsible for the message.
+ * @param {Function} sendResponse The response (bi-directional communication)
+ * to be used to send data.
+ */
 function handleMessage(request, sender, sendResponse) {
-    if (sender.url != browser.runtime.getURL("/devtools/panel/panel.html")) {
+    if (sender.url !== browser.runtime.getURL("/devtools/panel/panel.html")) {
         return;
     }
     browser.tabs.executeScript(
@@ -8,4 +18,6 @@ function handleMessage(request, sender, sendResponse) {
         });
 }
 
+// registers the message listener that will handle all of
+// the messages sent to theis background script
 chrome.runtime.onMessage.addListener(handleMessage);
